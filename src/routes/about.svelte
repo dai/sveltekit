819d0f573819fcd61cbec6312fd93eacc511ad02
osada <osada@gihyo.co.jp>
2022-02-09 11:31:01 +0900

    aboutを編集

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -22,17 +22,17 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		<a href="https://kit.svelte.dev">SvelteKit</a> app を使用したサンプルです。
+		シンプルなカウンター、TODOがあります:
 	</p>
 
-	<!-- TODO lose the @next! -->
+	<!-- TODO lose the @next! 
 	<pre>npm init svelte@next</pre>
+	-->
 
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		さてここでは、vercelを用いたビルドについて記します。
+		Githubにプロジェクトを作りpushしたらvercelが自動でビルド、デプロイを行います
 	</p>
 
 	<p>
